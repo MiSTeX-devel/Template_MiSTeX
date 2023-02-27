8dc22b75ec7bfbdb291b8a40805d843339e32090
Hans Baier <hansfbaier@gmail.com>
2023-02-27 10:36:47 +0700

    connect FPGA core reset line

--- sys/sys_top.v
+++ sys/sys_top.v
@@ -117,6 +117,7 @@ module sys_top
 	input 	      HPS_FPGA_ENABLE,
 	input         HPS_OSD_ENABLE,
 	input         HPS_IO_ENABLE,
+	input         HPS_CORE_RESET,
 	output  [3:0] DEBUG
 );
 
@@ -559,7 +560,7 @@ end
 
 ///////////////////////////  RESET  ///////////////////////////////////
 
-wire reset_req = 0;
+wire reset_req = HPS_CORE_RESET;
 
 ////////////////////  SYSTEM MEMORY & SCALER  /////////////////////////
 
