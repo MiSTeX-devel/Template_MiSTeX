abad2d5cc22bd74a9177084eda061296e26672e8
Hans Baier <hansfbaier@gmail.com>
2023-05-01 16:15:13 +0700

    make audio pll verilog compatible with non-cyclone v devices

--- sys/pll_audio_0002.v
+++ sys/pll_audio_0002.v
@@ -1,19 +1,13 @@
 `timescale 1ns/10ps
 module  pll_audio_0002(
 
-	// interface 'refclk'
 	input wire refclk,
-
-	// interface 'reset'
 	input wire rst,
-
-	// interface 'outclk0'
 	output wire outclk_0,
-
-	// interface 'locked'
 	output wire locked
 );
 
+`ifdef CYCLONEV
 	altera_pll #(
 		.fractional_vco_multiplier("true"),
 		.reference_clock_frequency("50.0 MHz"),
@@ -32,5 +26,30 @@ module  pll_audio_0002(
 		.fbclk	(1'b0),
 		.refclk	(refclk)
 	);
+`else // not CYCLONEV
+
+// TODO: make it reconfigurable
+ALTPLL #(
+	.BANDWIDTH_TYPE("AUTO"),
+	.CLK0_DIVIDE_BY(7'd59),
+	.CLK0_DUTY_CYCLE(6'd50),
+	.CLK0_MULTIPLY_BY(8'd29),
+	.CLK0_PHASE_SHIFT(1'd0),
+	.COMPENSATE_CLOCK("CLK0"),
+	.INCLK0_INPUT_FREQUENCY(24'd20000),
+	.OPERATION_MODE("NORMAL")
+) ALTPLL_HDMI (
+	.ARESET(1'd0),
+	.CLKENA(5'd31),
+	.EXTCLKENA(4'd15),
+	.FBIN(1'd1),
+	.INCLK(refclk),
+	.PFDENA(1'd1),
+	.PLLENA(1'd1),
+	.CLK({outclk_0}),
+	.LOCKED(locked)
+);
+
+`endif // CYCLONEV
 endmodule
 
