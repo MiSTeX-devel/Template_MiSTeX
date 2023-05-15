d24b75ba59a59535575ff51d538a39620c10f8ce
Hans Baier <hansfbaier@gmail.com>
2023-05-15 08:46:31 +0700

    Add MiSTeX HDMI conditionals

--- sys/sys_top.v
+++ sys/sys_top.v
@@ -48,6 +48,8 @@ module sys_top #(
 	output        HDMI_TX_VS,
 	
 	input         HDMI_TX_INT,
+
+	input         HDMI_CLK_IN,
 `endif
 
 	//////////// SDR ///////////
@@ -873,6 +875,7 @@ always @(posedge clk_vid) begin : video_calc_block
 	vmax <= vmaxi;
 end
 
+`ifndef MISTEX_HDMI
 `ifndef MISTER_DEBUG_NOHDMI
 wire [15:0] lltune;
 pll_hdmi_adj pll_hdmi_adj
@@ -894,7 +897,8 @@ pll_hdmi_adj pll_hdmi_adj
 );
 `else
 	assign led_locked = 0;
-`endif
+`endif // MISTER_DEBUG_NOHDMI
+`endif // MISTEX_HDMI
 
 wire [63:0] pal_data;
 wire [47:0] pal_d = {pal_data[55:32], pal_data[23:0]};
@@ -918,6 +922,7 @@ end
 /////////////////////////  HDMI output  /////////////////////////////////
 `ifndef MISTER_DEBUG_NOHDMI
 wire hdmi_clk_out;
+`ifndef MISTEX_HDMI
 pll_hdmi pll_hdmi
 (
 	.refclk(FPGA_CLK1_50),
@@ -926,7 +931,10 @@ pll_hdmi pll_hdmi
 	.reconfig_from_pll(reconfig_from_pll),
 	.outclk_0(hdmi_clk_out)
 );
-`endif
+`else // MISTEX_HDMI
+assign hdmi_clk_out = HDMI_CLK_IN;
+`endif // MISTEX_HDMI
+`endif // MISTER_DEBUG_NOHDMI
 
 //1920x1080@60 PCLK=148.5MHz CEA
 reg  [11:0] WIDTH   = 1920;
@@ -950,6 +958,7 @@ reg   [5:0] adj_address;
 reg  [31:0] adj_data;
 
 `ifndef MISTER_DEBUG_NOHDMI
+`ifndef MISTEX_HDMI
 pll_cfg pll_cfg
 (
 	.mgmt_clk(FPGA_CLK1_50),
@@ -1003,12 +1012,12 @@ always @(posedge FPGA_CLK1_50) begin : gotd_block
 	old_wait <= adj_waitrequest;
 	if(old_wait & ~adj_waitrequest & gotd) cfg_ready <= 1;
 end
-
 `else
-
 wire cfg_ready = 1;
-
-`endif
+`endif // MISTEX_HDMI
+`else
+wire cfg_ready = 1;
+`endif  // MISTER_DEBUG_NOHDMI
 
 wire hdmi_config_done;
 `ifdef HARDWARE_HDMI_INIT
@@ -1044,7 +1053,7 @@ cyclonev_hps_interface_peripheral_i2c hdmi_i2c
 
 `ifdef MISTER_FB
 reg dis_output;
-always @(posedge clk_hdmi) begin
+always @(posedge clk_hdmi) begin: fb_block
 	reg dis;
 	dis <= fb_force_blank & ~LFB_EN;
 	dis_output <= dis;
