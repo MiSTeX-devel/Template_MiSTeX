fd4549afe83b2a0a68a8cefba3d24142e984d31d
Hans Baier <foss@hans-baier.de>
2024-04-06 09:31:46 +0700

    hps_interface instantiation: fix gp_in

--- sys/sys_top.v
+++ sys/sys_top.v
@@ -318,7 +318,7 @@ wire [31:0] core_magic = {24'h5CA623, core_type};
 
 wire io_strobe;
 hps_interface hps_interface (
-	.gp_in(io_dout),
+	.gp_in(gp_in),
 	.gp_out(gp_out),
 	.io_strobe(io_strobe),
 
